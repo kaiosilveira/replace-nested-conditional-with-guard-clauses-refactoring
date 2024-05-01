1cdc03cbc2a88c361a037d14d29551944d756d1c
Kaio Silveira <silveira.kaio@icloud.com>
2024-05-01 16:53:22 +0100

    reversing-conds: remove temp `result` variable

--- src/reversing-conditions/index.js
+++ src/reversing-conditions/index.js
@@ -1,7 +1,5 @@
 export function adjustedCapital(anInvestment) {
-  let result = 0;
   if (anInvestment.capital <= 0 || anInvestment.interestRate <= 0 || anInvestment.duration <= 0)
-    return result;
-  result = (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
-  return result;
+    return 0;
+  return (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
 }
