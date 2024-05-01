168a89aa5691e5b41b164ab6824a71abc34910ee
Kaio Silveira <silveira.kaio@icloud.com>
2024-05-01 16:48:18 +0100

    reversing-conds: reverse `capital <= 0` condition

--- src/reversing-conditions/index.js
+++ src/reversing-conditions/index.js
@@ -1,9 +1,9 @@
 export function adjustedCapital(anInvestment) {
   let result = 0;
-  if (anInvestment.capital > 0) {
-    if (anInvestment.interestRate > 0 && anInvestment.duration > 0) {
-      result = (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
-    }
+  if (anInvestment.capital <= 0) return result;
+  if (anInvestment.interestRate > 0 && anInvestment.duration > 0) {
+    result = (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
   }
+
   return result;
 }
