5347529c30c968a09c759bb8fd14c385fecaaaa8
Kaio Silveira <silveira.kaio@icloud.com>
2024-05-01 16:49:48 +0100

    reversing-conds: reverse '`interestRate` and `duration` are positive' condition

--- src/reversing-conditions/index.js
+++ src/reversing-conditions/index.js
@@ -1,9 +1,7 @@
 export function adjustedCapital(anInvestment) {
   let result = 0;
   if (anInvestment.capital <= 0) return result;
-  if (anInvestment.interestRate > 0 && anInvestment.duration > 0) {
-    result = (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
-  }
-
+  if (!(anInvestment.interestRate > 0 && anInvestment.duration > 0)) return result;
+  result = (anInvestment.income / anInvestment.duration) * anInvestment.adjustmentFactor;
   return result;
 }
